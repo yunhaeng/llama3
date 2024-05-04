53e43c2d37dbe2dca2871cfd5958b53fd3dea948
yunhaeng <dkfk591@gmail.com>
2024-05-04 17:42:09 +0900

    Add annotation on setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 
+#llama3 패키지 구성을 위해 setuptools 라이브러리를 활용.
 from setuptools import find_packages, setup
 
-
+#requirements.txt 내의 필수 패키지를 리스트 형식으로 리턴하는 함수.
 def get_requirements(path: str):
     return [l.strip() for l in open(path)]
 
-
+#setup 함수로 llama3 패키지를 생성.
 setup(
     name="llama3",
     version="0.0.1",
