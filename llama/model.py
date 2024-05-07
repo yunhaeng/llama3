379d98f96eef52e7c5c4011f0b76dae5de760d7a
yunhaeng <dkfk591@gmail.com>
2024-05-07 18:09:09 +0900

    Add explanation of precompute_freqs_cis

--- llama/model.py
+++ llama/model.py
@@ -47,7 +47,7 @@ class RMSNorm(torch.nn.Module):
         output = self._norm(x.float()).type_as(x)
         return output * self.weight
 
-
+#llama3의 positional encoding
 def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
     t = torch.arange(end, device=freqs.device, dtype=torch.float32)
